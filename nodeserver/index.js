187c0749caf8fe60c6ec8902c3befb235fe1ade7
Drash09 <117528113+Drash09@users.noreply.github.com>
2023-04-14 15:13:12 +0530

    df

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,7 +1,7 @@
 // const io = require('socket.io')(8000)
 const io = require('socket.io')(8000, {
     cors: {
-      origin: '*',
+      origin: '*', 
     }
   });
 
@@ -22,4 +22,26 @@ io.on('connection',socket =>{
     });
 
 })
-               
+// Node Serever*********************************************************
+const express = require('express');
+const path = require('path');  
+app = express();
+const port = 3000;
+app.use('/static',express.static('../static'));
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'../')); 
+app.get('/',(req,res)=>{
+    res.status(200).render('home.pug'); 
+})  
+app.get('/call',(req,res)=>{
+    res.status(200).render('call.pug'); 
+}) 
+app.get('/chat',(req,res)=>{
+    res.status(200).render('chat.pug'); 
+}) 
+app.get('/about',(req,res)=>{
+    res.status(200).render('about.pug'); 
+}) 
+app.listen(port,()=>{
+    console.log(`site is Running on http://localhost:${port}/`); 
+}) 
